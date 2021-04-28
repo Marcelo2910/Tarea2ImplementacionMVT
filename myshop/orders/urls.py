bdf912cc4377e9f451f286b0092df3f5bed9a3ef
Marcelo Lira Montalvo <rammstolomeo@gmail.com>
2021-04-27 19:36:28 -0500

    se subio el codigo

---
+++ myshop/orders/urls.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------
+# Archivo: urls.py
+#
+# Descripción:
+#   En este archivo se definen las urls de la app de las órdenes.
+#
+#   Cada url debe tener la siguiente estructura:
+#
+#   path( url, vista, nombre_url )
+#
+#-------------------------------------------------------------------------
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('create/', views.order_create, name='order_create'),
+    path('list/', views.order_list, name='order_list'),
+    path('cancel/<int:id>', views.order_cancel, name='order_cancel'),
+]
